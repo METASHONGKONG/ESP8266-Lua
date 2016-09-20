adfbc3b3d19ea1580ebe3532ba55bd1daf0d42e7
Ken Lai <metas.ai2.tutorial@gmail.com>
2016-09-20 17:17:44 +0800

    Move all python into another repositories

--- ESP8266/NodeOne_Python/test/analog.py
+++
@@ -1,4 +0,0 @@
-from machine import ADC
-arr = [16,5,4,0,2,14,12,13,15,3,1,9,10] #GPIO->PIN
-adc = ADC(0)
-print(adc.read())
--- ESP8266/NodeOne_Python/test/digital.py
+++
@@ -1,4 +0,0 @@
-from machine import Pin
-arr = [16,5,4,0,2,14,12,13,15,3,1,9,10] #GPIO->PIN
-P0 = Pin(arr[0],Pin.OUT)                                       
-P0.value(0) #0:LOW,1:HIGH
